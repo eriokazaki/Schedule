e3e56635b850e78e8c50a9883ae1d7325e5e0552
Eri Okazaki <eri@ErinoMacBook-Pro.local>
2023-04-17 17:00:44 +0900

    #{全部移し替えました}

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,59 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+ 
+  def create
+    @post = Post.new(post_params)
+
+     if @post.save
+      flash[:success] = "スケジュールを新規登録しました"
+       redirect_to posts_path
+     else
+      flash.now[:failure] = "スケジュールを作成できませんでした"
+      render :new
+     end
+  end
+ 
+  def show
+    @post = Post.find(params[:id])
+  end
+ 
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "スケジュールID「#{@post.id}」の情報を更新しました"
+      redirect_to :post
+    else
+      flash.now[:failure] ="スケジュールID「#{@post.id}」の情報を更新できませんでした"
+      render "edit"
+    end
+  end
+ 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:failure] = "スケジュールを削除しました"
+    redirect_to :posts
+  end
+  
+  private 
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :start_at,
+      :end_at,
+      :is_all_day,
+      :memo
+    )
+  end
+end
